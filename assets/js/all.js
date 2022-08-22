3aa3c260fa8ca077071362a4ed1da4b174550a35
lingling <zzxcv741@hotmail.com>
2022-08-22 22:04:27 +0800

    Update 2022-08-22T14:04:23.812Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,8 +1,34 @@
 "use strict";
 
 $(document).ready(function () {
-  $('#menu-toggle').on('click', function () {
-    $('#mobile-menu').slideToggle();
+  $("#menu-toggle").on("click", function () {
+    $("#mobile-menu").slideToggle();
   });
+});
+"use strict";
+
+var _swiper = _interopRequireDefault(require("swiper"));
+
+require("swiper/css");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var swiper = new _swiper["default"]('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar'
+  }
 });
 //# sourceMappingURL=all.js.map
