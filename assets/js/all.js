7413f9cb0a18edbb44130539660186602fd8c141
lingling <zzxcv741@hotmail.com>
2022-08-28 15:13:36 +0800

    Update 2022-08-28T07:13:32.632Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -7,28 +7,29 @@ $(document).ready(function () {
 });
 "use strict";
 
-var _swiper = _interopRequireDefault(require("swiper"));
-
-require("swiper/css");
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-var swiper = new _swiper["default"]('.swiper', {
+// // import Swiper bundle with all modules installed
+//  import Swiper from 'swiper/bundle';
+//  // import styles bundle
+//  import '../style/swiper.css';
+var swiper = new Swiper(".swiper", {
   // Optional parameters
-  direction: 'vertical',
-  loop: true,
+  // direction: 'vertical',
+  loop: false,
+  autoplay: {
+    delay: 3000
+  },
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination'
+    el: ".swiper-pagination"
   },
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
   },
   // And if we need scrollbar
   scrollbar: {
-    el: '.swiper-scrollbar'
+    el: ".swiper-scrollbar"
   }
 });
 //# sourceMappingURL=all.js.map
